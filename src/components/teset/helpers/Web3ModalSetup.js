fe95028517a46bef74d97e0fbdca192191ef2f5d
Benjamin Shafii <ben@prologe.io>
2022-10-15 13:25:21 +0000

    feat: ability to render a single contract

--- src/components/teset/helpers/Web3ModalSetup.js
+++ src/components/teset/helpers/Web3ModalSetup.js
@@ -1,7 +1,5 @@
-import Portis from "@portis/web3";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import Authereum from "authereum";
-import Fortmatic from "fortmatic";
 import WalletLink from "walletlink";
 import Web3Modal from "web3modal";
 import { ALCHEMY_KEY, INFURA_ID } from "../constants";
@@ -40,52 +38,36 @@ const web3ModalSetup = () =>
           },
         },
       },
-      portis: {
-        display: {
-          logo: "https://user-images.githubusercontent.com/9419140/128913641-d025bc0c-e059-42de-a57b-422f196867ce.png",
-          name: "Portis",
-          description: "Connect to Portis App",
-        },
-        package: Portis,
-        options: {
-          id: "6255fb2b-58c8-433b-a2c9-62098c05ddc9",
-        },
+    },
+    // torus: {
+    //   package: Torus,
+    //   options: {
+    //     networkParams: {
+    //       host: "https://localhost:8545", // optional
+    //       chainId: 1337, // optional
+    //       networkId: 1337 // optional
+    //     },
+    //     config: {
+    //       buildEnv: "development" // optional
+    //     },
+    //   },
+    // },
+    "custom-walletlink": {
+      display: {
+        logo: "https://play-lh.googleusercontent.com/PjoJoG27miSglVBXoXrxBSLveV6e3EeBPpNY55aiUUBM9Q1RCETKCOqdOkX2ZydqVf0",
+        name: "Coinbase",
+        description: "Connect to Coinbase Wallet (not Coinbase App)",
       },
-      fortmatic: {
-        package: Fortmatic, // required
-        options: {
-          key: "pk_live_5A7C91B2FC585A17", // required
-        },
-      },
-      // torus: {
-      //   package: Torus,
-      //   options: {
-      //     networkParams: {
-      //       host: "https://localhost:8545", // optional
-      //       chainId: 1337, // optional
-      //       networkId: 1337 // optional
-      //     },
-      //     config: {
-      //       buildEnv: "development" // optional
-      //     },
-      //   },
-      // },
-      "custom-walletlink": {
-        display: {
-          logo: "https://play-lh.googleusercontent.com/PjoJoG27miSglVBXoXrxBSLveV6e3EeBPpNY55aiUUBM9Q1RCETKCOqdOkX2ZydqVf0",
-          name: "Coinbase",
-          description: "Connect to Coinbase Wallet (not Coinbase App)",
-        },
-        package: walletLinkProvider,
-        connector: async (provider, _options) => {
-          await provider.enable();
-          return provider;
-        },
-      },
-      authereum: {
-        package: Authereum, // required
+      package: walletLinkProvider,
+      connector: async (provider, _options) => {
+        await provider.enable();
+        return provider;
       },
     },
+    authereum: {
+      package: Authereum, // required
+    },
+  },
   });
 
 export default web3ModalSetup;
--- src/pages/new.js
+++ src/pages/new.js
@@ -1,6 +1,10 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit"
 import Header from "components/Header"
 import { Contract } from "components/teset/components"
+import { useContractLoader, useContractReader, useDexEthPrice } from 'eth-hooks';
+import { useNetwork, useProvider, useSigner } from "wagmi";
+
+
 
 
 
@@ -13,23 +17,619 @@ export default function Home() {
     )
 }
 
+const DAIABI = [
+    {
+        inputs: [
+            {
+                internalType: "uint256",
+                name: "chainId_",
+                type: "uint256",
+            },
+        ],
+        payable: false,
+        stateMutability: "nonpayable",
+        type: "constructor",
+    },
+    {
+        anonymous: false,
+        inputs: [
+            {
+                indexed: true,
+                internalType: "address",
+                name: "src",
+                type: "address",
+            },
+            {
+                indexed: true,
+                internalType: "address",
+                name: "guy",
+                type: "address",
+            },
+            {
+                indexed: false,
+                internalType: "uint256",
+                name: "wad",
+                type: "uint256",
+            },
+        ],
+        name: "Approval",
+        type: "event",
+    },
+    {
+        anonymous: true,
+        inputs: [
+            {
+                indexed: true,
+                internalType: "bytes4",
+                name: "sig",
+                type: "bytes4",
+            },
+            {
+                indexed: true,
+                internalType: "address",
+                name: "usr",
+                type: "address",
+            },
+            {
+                indexed: true,
+                internalType: "bytes32",
+                name: "arg1",
+                type: "bytes32",
+            },
+            {
+                indexed: true,
+                internalType: "bytes32",
+                name: "arg2",
+                type: "bytes32",
+            },
+            {
+                indexed: false,
+                internalType: "bytes",
+                name: "data",
+                type: "bytes",
+            },
+        ],
+        name: "LogNote",
+        type: "event",
+    },
+    {
+        anonymous: false,
+        inputs: [
+            {
+                indexed: true,
+                internalType: "address",
+                name: "src",
+                type: "address",
+            },
+            {
+                indexed: true,
+                internalType: "address",
+                name: "dst",
+                type: "address",
+            },
+            {
+                indexed: false,
+                internalType: "uint256",
+                name: "wad",
+                type: "uint256",
+            },
+        ],
+        name: "Transfer",
+        type: "event",
+    },
+    {
+        constant: true,
+        inputs: [],
+        name: "DOMAIN_SEPARATOR",
+        outputs: [
+            {
+                internalType: "bytes32",
+                name: "",
+                type: "bytes32",
+            },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        constant: true,
+        inputs: [],
+        name: "PERMIT_TYPEHASH",
+        outputs: [
+            {
+                internalType: "bytes32",
+                name: "",
+                type: "bytes32",
+            },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        constant: true,
+        inputs: [
+            {
+                internalType: "address",
+                name: "",
+                type: "address",
+            },
+            {
+                internalType: "address",
+                name: "",
+                type: "address",
+            },
+        ],
+        name: "allowance",
+        outputs: [
+            {
+                internalType: "uint256",
+                name: "",
+                type: "uint256",
+            },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        constant: false,
+        inputs: [
+            {
+                internalType: "address",
+                name: "usr",
+                type: "address",
+            },
+            {
+                internalType: "uint256",
+                name: "wad",
+                type: "uint256",
+            },
+        ],
+        name: "approve",
+        outputs: [
+            {
+                internalType: "bool",
+                name: "",
+                type: "bool",
+            },
+        ],
+        payable: false,
+        stateMutability: "nonpayable",
+        type: "function",
+    },
+    {
+        constant: true,
+        inputs: [
+            {
+                internalType: "address",
+                name: "",
+                type: "address",
+            },
+        ],
+        name: "balanceOf",
+        outputs: [
+            {
+                internalType: "uint256",
+                name: "",
+                type: "uint256",
+            },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        constant: false,
+        inputs: [
+            {
+                internalType: "address",
+                name: "usr",
+                type: "address",
+            },
+            {
+                internalType: "uint256",
+                name: "wad",
+                type: "uint256",
+            },
+        ],
+        name: "burn",
+        outputs: [],
+        payable: false,
+        stateMutability: "nonpayable",
+        type: "function",
+    },
+    {
+        constant: true,
+        inputs: [],
+        name: "decimals",
+        outputs: [
+            {
+                internalType: "uint8",
+                name: "",
+                type: "uint8",
+            },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        constant: false,
+        inputs: [
+            {
+                internalType: "address",
+                name: "guy",
+                type: "address",
+            },
+        ],
+        name: "deny",
+        outputs: [],
+        payable: false,
+        stateMutability: "nonpayable",
+        type: "function",
+    },
+    {
+        constant: false,
+        inputs: [
+            {
+                internalType: "address",
+                name: "usr",
+                type: "address",
+            },
+            {
+                internalType: "uint256",
+                name: "wad",
+                type: "uint256",
+            },
+        ],
+        name: "mint",
+        outputs: [],
+        payable: false,
+        stateMutability: "nonpayable",
+        type: "function",
+    },
+    {
+        constant: false,
+        inputs: [
+            {
+                internalType: "address",
+                name: "src",
+                type: "address",
+            },
+            {
+                internalType: "address",
+                name: "dst",
+                type: "address",
+            },
+            {
+                internalType: "uint256",
+                name: "wad",
+                type: "uint256",
+            },
+        ],
+        name: "move",
+        outputs: [],
+        payable: false,
+        stateMutability: "nonpayable",
+        type: "function",
+    },
+    {
+        constant: true,
+        inputs: [],
+        name: "name",
+        outputs: [
+            {
+                internalType: "string",
+                name: "",
+                type: "string",
+            },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        constant: true,
+        inputs: [
+            {
+                internalType: "address",
+                name: "",
+                type: "address",
+            },
+        ],
+        name: "nonces",
+        outputs: [
+            {
+                internalType: "uint256",
+                name: "",
+                type: "uint256",
+            },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        constant: false,
+        inputs: [
+            {
+                internalType: "address",
+                name: "holder",
+                type: "address",
+            },
+            {
+                internalType: "address",
+                name: "spender",
+                type: "address",
+            },
+            {
+                internalType: "uint256",
+                name: "nonce",
+                type: "uint256",
+            },
+            {
+                internalType: "uint256",
+                name: "expiry",
+                type: "uint256",
+            },
+            {
+                internalType: "bool",
+                name: "allowed",
+                type: "bool",
+            },
+            {
+                internalType: "uint8",
+                name: "v",
+                type: "uint8",
+            },
+            {
+                internalType: "bytes32",
+                name: "r",
+                type: "bytes32",
+            },
+            {
+                internalType: "bytes32",
+                name: "s",
+                type: "bytes32",
+            },
+        ],
+        name: "permit",
+        outputs: [],
+        payable: false,
+        stateMutability: "nonpayable",
+        type: "function",
+    },
+    {
+        constant: false,
+        inputs: [
+            {
+                internalType: "address",
+                name: "usr",
+                type: "address",
+            },
+            {
+                internalType: "uint256",
+                name: "wad",
+                type: "uint256",
+            },
+        ],
+        name: "pull",
+        outputs: [],
+        payable: false,
+        stateMutability: "nonpayable",
+        type: "function",
+    },
+    {
+        constant: false,
+        inputs: [
+            {
+                internalType: "address",
+                name: "usr",
+                type: "address",
+            },
+            {
+                internalType: "uint256",
+                name: "wad",
+                type: "uint256",
+            },
+        ],
+        name: "push",
+        outputs: [],
+        payable: false,
+        stateMutability: "nonpayable",
+        type: "function",
+    },
+    {
+        constant: false,
+        inputs: [
+            {
+                internalType: "address",
+                name: "guy",
+                type: "address",
+            },
+        ],
+        name: "rely",
+        outputs: [],
+        payable: false,
+        stateMutability: "nonpayable",
+        type: "function",
+    },
+    {
+        constant: true,
+        inputs: [],
+        name: "symbol",
+        outputs: [
+            {
+                internalType: "string",
+                name: "",
+                type: "string",
+            },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        constant: true,
+        inputs: [],
+        name: "totalSupply",
+        outputs: [
+            {
+                internalType: "uint256",
+                name: "",
+                type: "uint256",
+            },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        constant: false,
+        inputs: [
+            {
+                internalType: "address",
+                name: "dst",
+                type: "address",
+            },
+            {
+                internalType: "uint256",
+                name: "wad",
+                type: "uint256",
+            },
+        ],
+        name: "transfer",
+        outputs: [
+            {
+                internalType: "bool",
+                name: "",
+                type: "bool",
+            },
+        ],
+        payable: false,
+        stateMutability: "nonpayable",
+        type: "function",
+    },
+    {
+        constant: false,
+        inputs: [
+            {
+                internalType: "address",
+                name: "src",
+                type: "address",
+            },
+            {
+                internalType: "address",
+                name: "dst",
+                type: "address",
+            },
+            {
+                internalType: "uint256",
+                name: "wad",
+                type: "uint256",
+            },
+        ],
+        name: "transferFrom",
+        outputs: [
+            {
+                internalType: "bool",
+                name: "",
+                type: "bool",
+            },
+        ],
+        payable: false,
+        stateMutability: "nonpayable",
+        type: "function",
+    },
+    {
+        constant: true,
+        inputs: [],
+        name: "version",
+        outputs: [
+            {
+                internalType: "string",
+                name: "",
+                type: "string",
+            },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        constant: true,
+        inputs: [
+            {
+                internalType: "address",
+                name: "",
+                type: "address",
+            },
+        ],
+        name: "wards",
+        outputs: [
+            {
+                internalType: "uint256",
+                name: "",
+                type: "uint256",
+            },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+    },
+]
+const contractConfig = {
+    1: {
+        contracts: {
+            DAI: {
+                address: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+                abi: DAIABI,
+            },
+        },
+    },
+}
+
 
 
 const Main = () => {
+    const { chain } = useNetwork()
+    const { data: signer } = useSigner()
+    const provider = useProvider({ chainId: 1 })
 
-    const price = useExchangeEthPrice(targetNetwork, mainnetProvider, mainnetProviderPollingTime);
 
+    // const price = useExchangeEthPrice(targetNetwork, mainnetProvider, mainnetProviderPollingTime);
+    const price = 100
+
+    // If you want to bring in the mainnet DAI contract it would look like:
+    const mainnetContracts = useContractLoader(provider, contractConfig);
+
+    // If you want to call a function on a new block
+    // useOnBlock(mainnetProvider, () => {
+    //   console.log(`⛓ A new mainnet block is here: ${mainnetProvider._lastBlockNumber}`);
+    // });
+
+    // Then read your DAI balance like:
+
+
+
+
+
+    console
     return <div>mains
-
         <Contract
-            name="YourContract"
-            price={price}
-            signer={userSigner}
-            provider={localProvider}
-            address={address}
-            blockExplorer={blockExplorer}
+            name="DAI"
+            customContract={mainnetContracts && mainnetContracts.contracts && mainnetContracts.contracts.DAI}
+            signer={signer}
+            provider={provider}
+            address={"0x6B175474E89094C44Da98b954EedeAC495271d0F"}
+            blockExplorer="https://etherscan.io/"
             contractConfig={contractConfig}
+            chainId={1}
         />
+
+
 
     </div>
 
